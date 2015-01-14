f3aab9b83abd55bbb7c5308393101c04ee983450
Adam Schodde <aaschodd@asu.edu>
2015-01-14 15:16:40 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+Amazon Amiibo Checker
+===================
+
+
+Check Amazon.com for additions to the Amiibo parent ASIN.
+
+Current the parent ASIN has 18 Amiibo under it. When that value changes, that means preorder Amiibos should be available for purchase. An email is sent out to let you know with a link to the parent ASIN Amiibo page.
+
+Configuration
+-----------------
+
+```
+git clone <repo> /path/to/dump/repo
+cd /path/to/dump/repo
+npm install
+cp config.js.template config.js
+vim config.js
+npm start
+```
+
+License
+-----------
+
+[MIT](http://brutalhonesty.mit-license.org/)
+[TL;DR](https://tldrlegal.com/license/mit-license)
---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var request = require('request');
+var aws2 = require('aws2');
+var nodemailer = require('nodemailer');
+var parser = require('xml2json');
+var config = require('./config');
+var ASIN = 'B00N49RAXO'; // Amiibo Parent ASIN
+var PARENT_URL = 'http://www.amazon.com/dp/B00N49RAXO';
+var options = {
+  host: 'webservices.amazon.com',
+  path: '/onca/xml?Service=AWSECommerceService&Operation=ItemLookup&Availability=Available&ResponseGroup=VariationMatrix&IdType=ASIN&ItemId='+ASIN+'&AssociateTag=foobar'
+};
+
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+      user: config.gmail.user,
+      pass: config.gmail.pass
+  }
+});
+
+function _email(link) {
+  var mailOptions = {
+    from: config.gmail.from,
+    to: config.gmail.to,
+    subject: config.gmail.subject,
+    text: link
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if(error) {
+      console.log(error);
+    }
+    console.log(info);
+  });
+}
+
+setInterval(function () {
+  aws2.sign(options, {
+    accessKeyId: config.aws.accessKeyId,
+    secretAccessKey: config.aws.secretAccessKey
+  });
+  request("https://" + options.host + options.path, function (error, resp, body) {
+    if(error) {
+      console.log(error);
+    }
+    if(resp.statusCode !== 200) {
+      console.log(body);
+    }
+    body = parser.toJson(body);
+    body = JSON.parse(body);
+    var amiiboCount = body['ItemLookupResponse']['Items']['Item']['Variations']['TotalVariations'];
+    if(amiiboCount !== 18) {
+      _email(PARENT_URL);
+    } else {
+      console.log('Nothing new :(');
+    }
+  });
+}, Math.floor(Math.random() * 5000) + 10000);
